2f6e51f809d703d5880bfd7907471a8c7ca3b28a
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2024-11-06 21:21:26 +1100

    Remove `optimise_solution_delta()`.
    Removed algorithm to increase difference between optimal and second-best solution because it will probably never be used.

--- pykp/knapsack.py
+++ pykp/knapsack.py
@@ -59,9 +59,6 @@ class Knapsack:
 	* load_from_json():
 		Loads a knapsack configuration from a provided .json file.
 
-	* optimise_solution_delta():
-		An experimental method to increase the difference between the best and second-best terminal nodes without altering Sahni-k.
-
     * summary() -> pd.DataFrame:
         Generates a summary DataFrame containing information about the knapsack state and solutions.
     """
@@ -69,8 +66,6 @@ class Knapsack:
 		self, 
 		items: np.ndarray[Item], 
 		capacity: int, 
-		solve_terminal_nodes: bool = False,
-		solve_only_optimal_node: bool = False,
 		load_from_json: bool = False, 
 		path_to_spec: str = None
 	):
@@ -80,8 +75,6 @@ class Knapsack:
         Parameters:
     		items (np.ndarray[Item]): An array of items for the knapsack problem.
     		capacity (int): The maximum weight capacity of the knapsack.
-			solve_terminal_nodes (bool, optional): Whether to find all terminal nodes. Default is False.
-			solve_only_optimal_node (bool, optional): Whether to find only the optimal node, and forgo finding terminal nodes. Default is False.
 			load_from_json (bool, optional): Whether to load the instance from a .json spec. Default is False.
 			path_to_spec (str, optional): Path to json spec file. Default is None.
         """
@@ -650,93 +643,6 @@ class Knapsack:
 			axes.set_ylabel("Number of solutions")
 			axes.set_xlabel("Solution value")
 			plt.show()
-
-
-	def optimise_solution_delta(
-		self, 
-		target_delta_ratio: float, 
-		target_sahni_k: int | None = None, 
-		iteration: int = 0,
-		max_iterations: int | None = None,
-		**kwargs,
-	):
-		"""
-		Increases the distance between the value of the optimal and best-inferior
-		solution, while keeping Sahni-k constant.
-
-		Args:
-			target_delta_ratio (float): early stopping criteria. The algorithm  will stop if the ratio between the optimal and best-inferior value drops below the `target_delta_ratio`.
-			target_sahni_k (int | None, optional): The Sahni-k value that the optimal solution should have. You should initialise the algorithm with a knapsack in which the target solution already has the target Sahni-K. If None, the algorithm will maintain the Sahni-k of the current optimal solution. Defaults to None.
-			max_iterations (int, optional): The maximum iterations to run the algorithm. If None, the algorithm runs until convergence. Defaults to None.
-		"""
-		preserve_density = kwargs.get("preserve_density", False)	
-		progress_bar = kwargs.get("progress_bar", None)
-		if progress_bar is None and max_iterations != None:
-			progress_bar = tqdm(total = max_iterations)
-		if progress_bar is not None:
-			progress_bar.update(1)
-		if max_iterations and iteration > max_iterations:
-			print("Maximum iterations reached.")
-			return
-
-		if not target_sahni_k:
-			target_sahni_k = self.calculate_sahni_k(self.optimal_nodes[0])
-
-		best_inferior_solution = self.terminal_nodes[len(self.optimal_nodes)]
-		unique_in_solution_items = [
-			self.optimal_nodes[0].items[i] 
-			for i, element in enumerate(self.optimal_nodes[0].state) 
-			if element == 1 and best_inferior_solution.state[i] != 1
-		]
-		unique_in_best_inferior_items = [
-			best_inferior_solution.items[i] 
-			for i, element in enumerate(best_inferior_solution.state) 
-			if element == 1 and self.optimal_nodes[0].state[i] != 1
-		]
-		candidate_items_to_change = [*unique_in_solution_items, *unique_in_best_inferior_items]
-		current_delta = self.optimal_nodes[0].value - best_inferior_solution.value 
-		for item in candidate_items_to_change:
-			if item in unique_in_solution_items:
-				value_step_size = 1
-				weight_step_size = 0
-			else: 
-				value_step_size = -1
-				weight_step_size = -1 * int(preserve_density)
-			
-			item.update_value(max(0, item.value + value_step_size))
-			item.update_weight(max(0, item.weight + weight_step_size))
-			self.solve(verbose=False)
-
-			best_inferior_solution = self.terminal_nodes[len(self.optimal_nodes)]
-			new_solution_sahni_k = self.calculate_sahni_k(self.optimal_nodes[0])
-			new_delta = self.optimal_nodes[0].value - best_inferior_solution.value 
-
-			if new_solution_sahni_k < target_sahni_k or new_delta < current_delta:
-				item.update_value(item.value - value_step_size)
-				item.update_weight(item.weight - weight_step_size)
-				self.solve(verbose=False) 
-				continue
-
-			elif new_delta >= current_delta:
-				if round(best_inferior_solution.value / self.optimal_nodes[0].value, 4) <= target_delta_ratio:
-					print("Target achieved.")
-					return
-				if progress_bar is None:
-					print(f"Iteration: {iteration}; delta: {new_delta}")
-				return self.optimise_solution_delta(
-					target_delta_ratio = target_delta_ratio,
-					target_sahni_k = target_sahni_k,
-					iteration = iteration + 1,
-					preserve_density = not preserve_density, 
-					max_iterations = max_iterations,
-					progress_bar = progress_bar,
-				)
-		print("\n".join([
-			"-----------------------------------------------------",
-			f"Optimiser converged after {iteration} iterations.",
-			f"Solution delta: {current_delta}",
-			"-----------------------------------------------------",
-		]))
 
 
 	def write_to_json(self, path: str): 
