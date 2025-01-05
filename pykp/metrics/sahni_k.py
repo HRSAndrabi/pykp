9add1682f869f4b4ff137852185dd620071409db
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2025-01-05 18:17:35 +1100

    refactor(metrics.sahni_k): take capacity as input.
    Take capacity as input instead of the whole knapsack problem
    instances. This makes calls to the metric cleaner.

--- pykp/metrics/sahni_k.py
+++ pykp/metrics/sahni_k.py
@@ -4,9 +4,9 @@ This module provides an implementation of the Sahni-K metric for evaluating arra
 Example:
 	To calculate the Sahni-k of the optimal solution to a knapsack problem instance, first solve the instance and then call the metric on the optimal arrangement::
     
-		from pyinstance_copy import Knapsack
-		from pyinstance_copy import Item
-		import pyinstance_copy.metrics as metrics
+		from pyinstance import Knapsack
+		from pyinstance import Item
+		import pyinstance.metrics as metrics
 
 		items = [
 		   Item(value=10, weight=5), 
@@ -17,7 +17,7 @@ Example:
 		instance = Knapsack(items=items, capacity=capacity)
 		await instance.solve()
 
-		sahni_k = metrics.sahni_k(instance.optimal_nodes[0], instance)
+		sahni_k = metrics.sahni_k(instance.optimal_nodes[0], capacity)
 		print(sahni_k)
 """
 
@@ -28,44 +28,44 @@ import itertools
 
 def sahni_k(
 	arrangement: Arrangement,
-	instance: Knapsack,
+	capacity: int,
 ) -> int:
 	"""
 	Calculates the Sahni-k value for a given arrangement.
 
 	Parameters:
 		arrangement (Arrangement): The arrangement for which to calculate Sahni-k.
-		instance (Knapsack): The knapsack instance for which the arrangement was generated.
+		capacity (int): The capacity of the knapsack.
 
 	Returns:
 		int: Sahni-k value.
 	"""
 	if not isinstance(arrangement, Arrangement):
 		raise ValueError("`arrangement` must be of type `Arrangement`.")
-	if not isinstance(instance, Knapsack):
-		raise ValueError("`instance` must be of type `Knapsack`.")
+	if arrangement.weight > capacity:
+		raise ValueError("The total weight of items included in the `Arrangement` exceeds the `capacity`.")
 	
-	instance_copy = Knapsack(items = instance.items, capacity = instance.capacity)
+	instance = Knapsack(items = arrangement.items, capacity = capacity)
 
 	for subset_size in range(0, len(arrangement.state)+1):
 		in_indexes = [i for i, element in enumerate(arrangement.state) if element == 1]
 		for subset in itertools.combinations(in_indexes, subset_size):
 			initial_state = np.array([int(i in subset) for i in range(0, len(arrangement.state))])
-			instance_copy.set_state(initial_state.copy())
+			instance.set_state(initial_state.copy())
 			
 			# Solve greedily
-			while not instance_copy.is_at_capacity:
+			while not instance.is_at_capacity:
 				out_items = [
-					instance_copy.items[i] 
+					instance.items[i] 
 					for i, element 
-					in enumerate(instance_copy.state) 
-					if element == 0 and instance_copy.items[i].weight + instance_copy.weight <= instance_copy.capacity
+					in enumerate(instance.state) 
+					if element == 0 and instance.items[i].weight + instance.weight <= instance.capacity
 				]
 				densities = [item.value/item.weight for item in out_items]
-				instance_copy.add(
+				instance.add(
 					out_items[densities.index(max(densities))]
 				)
 			# Check hamming distance between arrangement and current state
-			hamming_distance = sum(np.absolute(np.subtract(arrangement.state, instance_copy.state)))	
+			hamming_distance = sum(np.absolute(np.subtract(arrangement.state, instance.state)))	
 			if hamming_distance == 0:
 				return subset_size
