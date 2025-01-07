fb687cd069d52f38daa9d497dc1b9dcf7f3ad178
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2025-01-07 11:20:15 +1100

    chore: remove underscores from filename.

--- pykp/solvers/__init__.py
+++ pykp/solvers/__init__.py
@@ -1,7 +1,7 @@
 """
-PyKP is a package to provide tooling for sampling and solving instances of the 0-1 Knapsack Problem. It is licensed under the MIT License.
+This module contains the implementations of the solvers for the knapsack problem. 
 """
 
-from ._branch_and_bound import branch_and_bound
-from ._greedy import greedy
-from ._mzn_gecode import mzn_gecode
+from .branch_and_bound import branch_and_bound
+from .greedy import greedy
+from .mzn_gecode import mzn_gecode
