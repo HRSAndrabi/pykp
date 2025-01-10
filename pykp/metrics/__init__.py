47e2686bee73156d358b3bf8f400383ba52fa5bd
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2025-01-10 17:31:13 +1100

    refactor: move metrics package to module.

--- pykp/metrics/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This module contains various metrics for evaluating knapsack problem instances.
-"""
-
-from .phase_transition import phase_transition
-from .sahni_k import sahni_k
-
-__all__ = ["sahni_k", "phase_transition"]
