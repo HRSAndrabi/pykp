a8d9604fa062a58de2589bd5fcee50c3973d5692
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2025-01-11 21:49:12 +1100

    docs(arrangement): docstrings to numpydoc.

--- pykp/arrangement.py
+++ pykp/arrangement.py
@@ -22,15 +22,25 @@ from .item import Item
 
 
 class Arrangement:
-    """
-    Represents an arrangement of items for the knapsack problem.
+    """Represents an arrangement of items for the knapsack problem.
 
-    Parameters:
-        items (list[Item]): An array of items for the knapsack problem.
-        state (list[int]): Binary array indicating the inclusion or
-            exclusion of items in the arrangement.
-        value (int): The total value of items in the arrangement.
-        weight (int): The total weight of items in the arrangement.
+    Used to store the state of items in the knapsack, as well as the total
+    value and weight of items in the arrangement. The state is represented
+    as a binary array, where 1 indicates the item is in the arrangement, and
+    0 indicates it is not.
+
+
+    Parameters
+    ----------
+    items: list[Item]
+        An array of items for the knapsack problem.
+    state: list[int]
+        Binary array indicating the inclusion or exclusion of items in the
+        arrangement.
+    value: int
+        The total value of items in the arrangement.
+    weight int:
+        The total weight of items in the arrangement.
     """
 
     def __init__(
@@ -60,8 +70,9 @@ class Arrangement:
     @property
     def state(self) -> list[int]:
         """
-        The state of the arrangement. 1 indicates the item is in the
-        arrangement, 0 indicates it is not.
+        The state of the arrangement.
+
+        1 indicates the item is in the arrangement, 0 indicates it is not.
         """
         return list(self._state)
 
@@ -76,11 +87,12 @@ class Arrangement:
         return self._weight
 
     def __calculate_value(self):
-        """
-        Calculates the total value of items currently in the knapsack.
+        """Calculate total value of items currently in the knapsack.
 
-        Returns:
-            float: The total value of items in the knapsack.
+        Returns
+        -------
+        float
+            The total value of items in the knapsack.
         """
         return sum(
             [
@@ -91,11 +103,12 @@ class Arrangement:
         )
 
     def __calculate_weight(self):
-        """
-        Calculates the total weight of items currently in the knapsack.
+        """Calculate the total weight of items currently in the knapsack.
 
-        Returns:
-            float: The total weight of items in the knapsack.
+        Returns
+        -------
+        float
+            The total weight of items in the knapsack.
         """
         return sum(
             [
@@ -106,15 +119,19 @@ class Arrangement:
         )
 
     def __hash__(self):
+        """Hash the state of the arrangement."""
         return hash(tuple(self._state))
 
     def __eq__(self, other):
+        """Check if two arrangements are equal."""
         return np.array_equal(self._state, other.state)
 
     def __str__(self):
+        """Return a string representation of the arrangement."""
         state = int("".join(self._state.astype(int).astype(str)), 2)
         return f"(v: {self._value}, w: {self._weight}, s: {state})"
 
     def __repr__(self):
+        """Return a string representation of the arrangement."""
         state = int("".join(self._state.astype(int).astype(str)), 2)
         return f"(v: {self._value}, w: {self._weight}, s: {state})"
