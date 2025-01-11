b63f6b5aa8be43bfc04d2c1c1cfbc1cc53a1b0f8
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2025-01-11 19:01:25 +1100

    refactor(arrangement): add @property decorators.

--- pykp/arrangement.py
+++ pykp/arrangement.py
@@ -25,9 +25,9 @@ class Arrangement:
     """
     Represents an arrangement of items for the knapsack problem.
 
-    Attributes:
-        items (np.ndarray[Item]): An array of items for the knapsack problem.
-        state (np.ndarray[int]): Binary array indicating the inclusion or
+    Parameters:
+        items (list[Item]): An array of items for the knapsack problem.
+        state (list[int]): Binary array indicating the inclusion or
             exclusion of items in the arrangement.
         value (int): The total value of items in the arrangement.
         weight (int): The total weight of items in the arrangement.
@@ -35,26 +35,45 @@ class Arrangement:
 
     def __init__(
         self,
-        items: np.ndarray[Item],
-        state: np.ndarray[int],
+        items: list[Item],
+        state: list[int],
     ):
-        """
-        Initialises an Arrangement instance.
-
-        Args:
-        items (np.ndarray[Item]): An array of items for the knapsack problem.
-        state (np.ndarray[int]): Binary array indicating the inclusion or
-            exclusion of items in the arrangement.
-        capacity (int): The maximum weight capacity constraint for the
-            arrangement.
-        """
         if not np.all(np.isin(state, [0, 1])):
             raise ValueError("Elements of `state` must be 0 or 1.")
 
-        self.items = items
-        self.state = state
-        self.value = self.__calculate_value()
-        self.weight = self.__calculate_weight()
+        if not isinstance(items, np.ndarray):
+            items = np.array(items)
+
+        if not isinstance(state, np.ndarray):
+            state = np.array(state)
+
+        self._items = items
+        self._state = state
+        self._value = self.__calculate_value()
+        self._weight = self.__calculate_weight()
+
+    @property
+    def items(self) -> list[Item]:
+        """The items in the arrangement."""
+        return list(self._items)
+
+    @property
+    def state(self) -> list[int]:
+        """
+        The state of the arrangement. 1 indicates the item is in the
+        arrangement, 0 indicates it is not.
+        """
+        return list(self._state)
+
+    @property
+    def value(self) -> float:
+        """The total value of items in the arrangement."""
+        return self._value
+
+    @property
+    def weight(self) -> float:
+        """The total weight of items in the arrangement."""
+        return self._weight
 
     def __calculate_value(self):
         """
@@ -65,8 +84,8 @@ class Arrangement:
         """
         return sum(
             [
-                self.items[i].value
-                for i, inside in enumerate(self.state)
+                self._items[i].value
+                for i, inside in enumerate(self._state)
                 if bool(inside)
             ]
         )
@@ -80,22 +99,22 @@ class Arrangement:
         """
         return sum(
             [
-                self.items[i].weight
-                for i, inside in enumerate(self.state)
+                self._items[i].weight
+                for i, inside in enumerate(self._state)
                 if bool(inside)
             ]
         )
 
     def __hash__(self):
-        return hash(tuple(self.state))
+        return hash(tuple(self._state))
 
     def __eq__(self, other):
-        return np.array_equal(self.state, other.state)
+        return np.array_equal(self._state, other.state)
 
     def __str__(self):
-        state = int("".join(self.state.astype(int).astype(str)), 2)
-        return f"(v: {self.value}, w: {self.weight}, s: {state})"
+        state = int("".join(self._state.astype(int).astype(str)), 2)
+        return f"(v: {self._value}, w: {self._weight}, s: {state})"
 
     def __repr__(self):
-        state = int("".join(self.state.astype(int).astype(str)), 2)
-        return f"(v: {self.value}, w: {self.weight}, s: {state})"
+        state = int("".join(self._state.astype(int).astype(str)), 2)
+        return f"(v: {self._value}, w: {self._weight}, s: {state})"
