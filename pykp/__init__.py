1dcae0ca79e501161e8714bae22ef1c27240fb64
Hassan Andrabi <62787278+HRSAndrabi@users.noreply.github.com>
2025-01-12 15:21:52 +1100

    docs(pykp): docstrings to numpydoc.

--- pykp/__init__.py
+++ pykp/__init__.py
@@ -1,7 +1,4 @@
-"""
-PyKP is a package to provide tooling for sampling and solving instances of the
-0-1 Knapsack Problem. It is licensed under the MIT License.
-"""
+"""PyKP: A Python library for the Knapsack Problem."""
 
 from .arrangement import Arrangement
 from .item import Item
